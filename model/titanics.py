fdd1bdaa55deee7dd61795da06db670755f82dad
Hannah <hannahj.g.reyes@gmail.com>
2024-03-29 10:25:51 -0700

    polish

--- model/titanics.py
+++ model/titanics.py
@@ -189,6 +189,7 @@ titanic_data.drop(['alive', 'who', 'adult_male', 'class', 'embark_town', 'deck']
 titanic_data.dropna(inplace=True)
 titanic_data['sex'] = titanic_data['sex'].apply(lambda x: 1 if x == 'male' else 0)
 titanic_data['alone'] = titanic_data['alone'].apply(lambda x: 1 if x == True else 0)
+
 # Encode categorical variables
 enc = OneHotEncoder(handle_unknown='ignore')
 enc.fit(titanic_data[['embarked']])
@@ -197,10 +198,12 @@ cols = ['embarked_' + val for val in enc.categories_[0]]
 titanic_data[cols] = pd.DataFrame(onehot)
 titanic_data.drop(['embarked'], axis=1, inplace=True)
 titanic_data.dropna(inplace=True)
+
 # Split the data into training and testing sets
 X = titanic_data.drop('survived', axis=1)
 y = titanic_data['survived']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
 # Train a decision tree classifier
 dt = DecisionTreeClassifier()
 dt.fit(X_train, y_train)
